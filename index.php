39b1f5ee7eb537e8c9754cca17ee78ffae7e9e2a
Kavin Yao <kavinyao@gmail.com>
2013-05-08 21:35:04 +0800

    unify style

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query. 
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Starkers
- * @since Starkers 3.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
 <div id="main-wrapper">
     <div id="main" class="col-full">
         <div class="bisection">
@@ -32,9 +18,4 @@ get_header(); ?>
     </div>
 </div>
 
-
-<div id="footer-wrapper">
-    <div id="footer" class="col-full">
-        <?php get_footer(); ?>
-    </div>
-</div>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,42 +1,38 @@
-<?php
-/**
- * The Template for displaying all single posts.
- *
- * @package WordPress
- * @subpackage Starkers
- * @since Starkers 3.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
 <div id="main-wrapper">
     <div id="main" class="col-full">
-        <div class="bisection">
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        <div class="bisection">
+            <div class="section-left">&nbsp;</div>
+            <div class="section-right">
+                <div class="post-part">
+                    <h1><?php the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="bisection">
             <div class="section-left">
                 <?php portfolio_show_slide_images(); ?>
             </div>
             <div class="section-right">
-            <div class="post-part">
-                        <h1><?php the_title(); ?></h1>
-                            <?php //twentyten_posted_on(); ?>
-            </div>
-            <div class="post-part">
-                <?php the_content(); ?>
-                <?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
-            </div>
-            <div class="post-part">
-                <?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
-                            <?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
-                            <h2><?php printf( esc_attr__( 'About %s', 'twentyten' ), get_the_author() ); ?></h2>
-                            <?php the_author_meta( 'description' ); ?>
-                            <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
-                                <?php printf( __( 'View all posts by %s &rarr;', 'twentyten' ), get_the_author() ); ?>
-                            </a>
-                <?php endif; ?>
-            </div>
-        <?php endwhile; // end of the loop. ?>
+                <div class="post-part">
+                    <?php the_content(); ?>
+                    <?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+                </div>
+                <div class="post-part">
+                    <?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+                        <?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
+                        <h2><?php printf( esc_attr__( 'About %s', 'twentyten' ), get_the_author() ); ?></h2>
+                        <?php the_author_meta( 'description' ); ?>
+                        <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+                            <?php printf( __( 'View all posts by %s &rarr;', 'twentyten' ), get_the_author() ); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
+        <?php endwhile; // end of the loop. ?>
     </div>
 </div>
 
